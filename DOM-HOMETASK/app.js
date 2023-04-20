245a7a2572247b1029ca9ceb8064492be8238fd1
HasanBaghirov <7kbac95@code.edu.az>
2023-04-21 01:23:07 +0400

    Hazirdir

---
+++ DOM-HOMETASK/app.js
@@ -0,0 +1,103 @@
+const firstDiv = document.getElementById("firstDiv");
+
+//first task start
+
+// const classDiv = document.getElementsByClassName("classDiv");
+
+// firstDiv.style.cssText = `
+// width:300px;
+// height:300px;
+// background-color:yellow;
+
+// `;
+
+// classDiv[0].style.cssText = `
+// width:300px;
+// height:300px;
+// background-color:green;
+
+// `;
+
+// classDiv[1].style.cssText = `
+// width:300px;
+// height:300px;
+// background-color:black;
+
+// `;
+
+//first task end
+
+
+
+
+
+
+
+
+
+
+
+
+
+//second task start
+
+// firstDiv.style.cssText = `
+// width:300px;
+// height:300px;
+// background-color:green;
+// `;
+
+// const colorBtn = document.createElement("button");
+// colorBtn.innerText = "sari";
+// colorBtn.className = "sariReng";
+// firstDiv.appendChild(colorBtn);
+
+// const colorBtn1 = document.createElement("button");
+// colorBtn1.innerText = "qirmizi";
+// colorBtn1.className = "qirmiziReng";
+// firstDiv.appendChild(colorBtn1);
+
+// const colorBtn2 = document.createElement("button");
+// colorBtn2.innerText = "qara";
+// colorBtn2.className = "qaraReng";
+// firstDiv.appendChild(colorBtn2);
+
+// colorBtn.addEventListener("click", function () {
+//   firstDiv.style.backgroundColor = "yellow";
+// });
+
+// colorBtn1.addEventListener("click", function () {
+//   firstDiv.style.backgroundColor = "red";
+// });
+
+// colorBtn2.addEventListener("click", function () {
+//   firstDiv.style.backgroundColor = "black";
+// });
+
+//second task end
+
+
+
+
+
+
+
+
+
+//third task start
+
+// const randomBtn = document.createElement("button");
+// randomBtn.innerText = "randomColor";
+// randomBtn.className = "randomColor";
+// firstDiv.appendChild(randomBtn);
+
+// randomBtn.addEventListener("click", function () {
+//   const x = Math.floor(Math.random() * 256);
+//   const y = Math.floor(Math.random() * 256);
+//   const z = Math.floor(Math.random() * 256);
+//   const bgColor = "rgb(" + x + "," + y + "," + z + ")";
+//   console.log(bgColor);
+//   document.body.style.background = bgColor;
+// });
+
+//third task end
